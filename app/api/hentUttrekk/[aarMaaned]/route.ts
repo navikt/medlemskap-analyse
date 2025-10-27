843a6af793bf1adc954e2ee25e04fbc325c8579b
Leggenn <111862499+Leggenn@users.noreply.github.com>
2025-10-27 13:01:27 +0100

    tester oasis

--- app/api/hentUttrekk/[aarMaaned]/route.ts
+++ app/api/hentUttrekk/[aarMaaned]/route.ts
@@ -2,7 +2,9 @@ import { type NextRequest, NextResponse } from "next/server"
 import { getToken, validateToken, requestAzureOboToken } from "@navikt/oasis"
 
 const API_BASE_URL = "https://medlemskap-vurdering.intern.dev.nav.no"
-const SAGA_CLIENT = "api://39f402c0-7373-49e3-9e64-9669181f78d4/.default"
+//const SAGA_CLIENT = "api://39f402c0-7373-49e3-9e64-9669181f78d4/.default"
+const SAGA_CLIENT = "api://dev-gcp.medlemskap.medlemskap-saga/.default"
+
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ aarMaaned: string }> }) {
     try {
